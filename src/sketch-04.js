a460a1e6bd9b36bff174e45570d2c53ed7c3a9e0
Melissa <melissa.tortelli@gmail.com>
2021-11-28 18:52:16 -0300

    adding glyphs

--- src/sketch-04.js
+++ src/sketch-04.js
@@ -1,7 +1,9 @@
 const canvasSketch = require('canvas-sketch')
+const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 }
 
 let manager
@@ -12,7 +14,7 @@ const fontFamily = 'serif'
 const typeCanvas = document.createElement('canvas')
 const typeContext = typeCanvas.getContext('2d')
 
-const sketch = ({ context, width, height }) => {
+const sketch = ({ context, width, height, frame }) => {
   const cells = 14
   const cols = Math.floor(width / cells)
   const rows = Math.floor(height / cells)
@@ -56,6 +58,12 @@ const sketch = ({ context, width, height }) => {
 
     context.drawImage(typeCanvas, 0, 0)
 
+    context.fillStyle = 'black'
+    context.fillRect(0, 0, width, height)
+
+    context.textBaseline = 'middle'
+    context.textAlign = 'center'
+
     for (let i = 0; i < numCells; i++) {
       const col = i % cols
       const row = Math.floor(i / cols)
@@ -68,21 +76,38 @@ const sketch = ({ context, width, height }) => {
       const b = typeData[i * 4 + 2]
       const a = typeData[i * 4 + 3]
 
-      context.fillStyle = `rgb(${r}, ${g}, ${b})`
+      const glyph = getGlyph(a)
+
+      context.font = `${cells * 2}px ${fontFamily}`
+
+      context.fillStyle = 'white'
 
       context.save()
       context.translate(x, y)
-      // context.translate(cells * 0.5, cells * 0.5)
+      context.translate(cells * 0.5, cells * 0.5)
 
-      context.fillRect(0, 0, cells, cells)
+      // context.fillRect(0, 0, cells, cells)
 
       // context.beginPath()
       // context.arc(0, 0, cells * 0.5, 0, Math.PI * 2)
       // context.fill()
 
+      context.fillText(glyph, 0, 0)
+
       context.restore()
     }
   }
+}
+
+const getGlyph = (v) => {
+  if (v < 50) return ''
+  if (v < 100) return '.'
+  if (v < 150) return '-'
+  if (v < 200) return '+'
+
+  const glyphs = './='.split('')
+
+  return random.pick(glyphs)
 }
 
 const onKeyUp = (event) => {
